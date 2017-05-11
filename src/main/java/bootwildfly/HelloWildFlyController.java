e4ff8d35198760c2d6dbadfad938877d9bc1e0cb
350401 <pavan.rankireddy@gmail.com>
2017-05-11 19:43:36 +0530

    added env variables

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -9,6 +9,6 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly");
+        return ("Hello, SpringBoot on Wildfly" + " USERNAME : "+System.getenv("MYSQL_USER") +"  URL : "+ System.getenv("MYSQL_DB_URL"));
     }
 }
